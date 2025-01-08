274f3dade747a664da3a480e5d9b20b1110a972d
changfutao <changfutao@126.com>
2025-01-08 14:31:01 +0800

    RelativeSource 注释

---
+++ WPFLearn.BindingValidation/Data.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFLearn.BindingValidation
+{
+    public class Data
+    {
+		private int _value = 10;
+
+		public int Value 
+		{
+			get { return _value; }
+			set { 
+				if(value == 123)
+				{
+					throw new Exception("Exception 123");
+				}
+				_value = value;
+			}
+		}
+
+		private string _myName;
+
+		public string MyName
+		{
+			get { return _myName; }
+			set { _myName = value; }
+		}
+
+
+	}
+}
