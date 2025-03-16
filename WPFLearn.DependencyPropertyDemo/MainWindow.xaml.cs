2d5be0d7d6047fe229f7438fcdc2fa6197db8906
unknown <changfutao1989@126.com>
2025-03-16 19:45:23 +0800

    依赖属性

---
+++ WPFLearn.DependencyPropertyDemo/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFLearn.DependencyPropertyDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    class CustomBox: TextBox
+    {
+        public bool IsHighLight
+        {
+            get { return (bool)GetValue(IsHighLightProperty); }
+            set { SetValue(IsHighLightProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsHighLightProperty =
+            DependencyProperty.Register("IsHighLight", typeof(bool), typeof(CustomBox), new PropertyMetadata(false));
+
+
+    }
+}
