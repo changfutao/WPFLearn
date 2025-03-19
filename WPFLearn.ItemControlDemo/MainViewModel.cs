00f0d7bc93ab6d98c62ee09dc6befc7553233203
unknown <changfutao1989@126.com>
2025-03-19 20:47:02 +0800

    ItemControl

---
+++ WPFLearn.ItemControlDemo/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFLearn.ItemControlDemo
+{
+    internal class MainViewModel
+    {
+        public string[] names = new[] { "Jack", "Tom", "Lily", "Jerry", "Hameimei"}; 
+        public List<string> Names { get; set; }
+        public MainViewModel()
+        {
+            Names = new List<string>();
+            foreach (var item in Enumerable.Range(0, 100))
+            {
+                Names.Add(names[new Random().Next(5)] + item);
+            }
+        }
+    }
+}
