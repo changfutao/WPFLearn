1cd3201f1ba263f24a78d6c6a208f8d05f0d3c32
unknown <changfutao1989@126.com>
2025-04-28 20:37:44 +0800

    datagrid mvvm

---
+++ WPFLearn.DataGridDemo/DataGridFilter2.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFLearn.DataGridDemo
+{
+    /// <summary>
+    /// DataGridFilter2.xaml 的交互逻辑
+    /// </summary>
+    public partial class DataGridFilter2 : Window
+    {
+        private List<Employee> _employees;
+        private ICollectionView _collectionView;
+        public DataGridFilter2()
+        {
+            InitializeComponent();
+            _employees = new List<Employee>(Employee.FakeMany(10));
+            _collectionView = CollectionViewSource.GetDefaultView(_employees);
+            dgData.ItemsSource = _collectionView;
+            _collectionView.Filter = (item) =>
+            {
+                var em = item as Employee;
+                if (em != null)
+                {
+                    var text = filterTextBox.Text;
+                    return em.FirstName.Contains(text) || em.LastName.Contains(text);
+                }
+                return false;
+            };
+        }
+
+        private void filterTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            _collectionView.Refresh();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _employees.Add(Employee.FakeOne());
+            _collectionView.Refresh();
+        }
+    }
+}
---
+++ WPFLearn.DataGridDemo/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.ComponentModel;
+using System.Windows.Data;
+
+namespace WPFLearn.DataGridDemo
+{
+    public partial class MainViewModel: ObservableObject
+    {
+        private List<Employee> employees;
+        [ObservableProperty]
+        private ICollectionView collectionView;
+        [ObservableProperty]
+        private string filterText = "";
+        partial void OnFilterTextChanged(string value) => CollectionView.Refresh();
+        public MainViewModel()
+        {
+            employees = new List<Employee>(Employee.FakeMany(10));
+            CollectionView = CollectionViewSource.GetDefaultView(employees);
+            CollectionView.Filter = (item) =>
+            {
+                var em = item as Employee;
+                if(em != null)
+                {
+                    return em.FirstName.Contains(filterText) || em.LastName.Contains(filterText);
+                }
+                return false;
+            };
+        }
+        [RelayCommand]
+        public void AddEmployee()
+        {
+            employees.Add(Employee.FakeOne());
+            CollectionView.Refresh();
+        }
+    }
+}
